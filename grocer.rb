da76b71f6014a58f76ef3217b75fd25ca72983f5
github username <github email address>
2018-11-19 08:57:22 -0500

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,33 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  newCart = {}
+  cart.each do |item_w_attributes|
+    item.each do |item, attributes|
+      binding.pry
+    if newCart[item] = nil
+      newCart = item
+      newCart[item][:count] = 1
+    elsif newCart[item] = item
+
+    else
+      binding.pry
+      newCart[item] << attributes
+      newCart[item][:count] += 1
+    end
+  end
+  end
 end
 
+
 def apply_coupons(cart, coupons)
-  # code here
+  cart.each do |name, attributes|
+    if attributes[:clearance]
+      sale_price = attributes[:price] * 0.80
+      attributes[:price] = sale_price.round(2)
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
